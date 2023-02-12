a2b5c5ba6cf270d2589e63d63fc12b0978f4301e
Thomas Dolnet <thomas.dolnet@free.fr>
2023-02-12 14:53:56 +0100

    Pages fonctionnelles + page détails du programme

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import tkinter as tk
 import customtkinter as ctk
 from random import *
-Mdp=[]
+import pyperclip  
+
+
+
 class App(ctk.CTk):
     def __init__(self, *args, **kwargs):
         ctk.CTk.__init__(self, *args, **kwargs)
@@ -24,12 +27,13 @@ class App(ctk.CTk):
             nav, text="CreatePasswords Page", command=self.showCreatePasswords, fg_color="black"
         )
         btn_Pwds = ctk.CTkButton(nav, text="Passwords Page", command=self.showPasswords, fg_color="black")
+        btn_Details = ctk.CTkButton(nav, text="Détails", command=self.showDetails, fg_color="black")
         btn_Verifyer.pack(side="top", pady=10)
         btn_CreatePasswords.pack(side="top", pady=10)
         btn_Pwds.pack(side="top", pady=10)
-
+        btn_Details.pack(side="bottom", pady=10)
         self.frames = {}
-        for F in (CreatePasswordsPage, VerifyerPage, PasswordsPage):
+        for F in (CreatePasswordsPage, VerifyerPage, PasswordsPage, Details):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
             # OR F.__name__ pour que _name_ ne dépende pas d'une instance
@@ -51,6 +55,9 @@ class App(ctk.CTk):
     def showPasswords(self):
         self.showFrame(self.frames["PasswordsPage"])
 
+    def showDetails(self):
+        self.showFrame(self.frames["Details"])
+
 
 
 class CreatePasswordsPage(ctk.CTkFrame):
@@ -62,18 +69,32 @@ class CreatePasswordsPage(ctk.CTkFrame):
         self.controller = controller
         self.label = ctk.CTkLabel(self, text="CreatePasswordsPage")
         self.label.pack(side="top", fill="x")
-        self.btn = ctk.CTkButton(
-            self, text="Generate a password", command=self.Click_event
+        self.btn_generate = ctk.CTkButton(
+            self, text="Generate a password", command=self.Click_event_Dialog
         )
-        self.btn.pack(side="top", fill="x")
+        self.btn_generate.pack(side="top", fill="x")
         self.labelMotDePasse = ctk.CTkLabel(self, text="", fg_color="black")
-        self.labelMotDePasse.pack(side="bottom", fill="x")
+        self.labelMotDePasse.pack(side="top", fill="x", pady=250)
+        self.btn_copy = ctk.CTkButton(
+            self, text="Copier le mot de passe", command=pyperclip.copy(''.join([str(i) for i in Mdp]))
+        )
+        self.btn_copy.pack(side="bottom", fill="x", pady=10)
+        self.btn_paste = ctk.CTkButton(
+            self, text="Paste", command=self.Click_event_Paste
+        )
+        self.btn_paste.pack(side="bottom", fill="x")
+        self.label_paste = ctk.CTkLabel(self, text="", fg_color="red")
+        self.label_paste.pack(side="bottom", fill="x")
         
-    def Click_event(self):
+    def Click_event_Dialog(self):
         dialog = ctk.CTkInputDialog(text="Longueur du mot de passe", title="Password")
         MdpLongueur = int(dialog.get_input()) 
         self.labelMotDePasse.configure(text=mdp(MdpLongueur))
+        
     
+    def Click_event_Paste(self):
+        self.label_paste.configure(text=pyperclip.paste())
+
 
 
 class VerifyerPage(ctk.CTkFrame):
@@ -83,6 +104,24 @@ class VerifyerPage(ctk.CTkFrame):
         self.controller = controller
         label = ctk.CTkLabel(self, text="VerifyerPage")
         label.pack(side="top", fill="x")
+        self.entry_verif = ctk.CTkEntry(self, placeholder_text="Mot de passe à tester", fg_color="black")
+        self.entry_verif.pack(side="bottom", fill="x")
+        self.btn_verif = ctk.CTkButton(
+            self, text="Vérifier", command=self.Click_event_Verif
+        )
+        self.btn_verif.pack(side="top", fill="x")
+        self.progressbar_verif = ctk.CTkProgressBar(self, progress_color="red")
+        self.progressbar_verif.pack(side="top", fill="x", pady=15)
+        self.progressbar_verif.set(value=0)
+
+
+    def Click_event_Verif(self):
+        verifmdp = self.entry_verif.get()
+        s=verif(verifmdp)
+        self.progressbar_verif.set(value=s)
+        color(self, s)
+
+
 
 
 class PasswordsPage(ctk.CTkFrame):
@@ -93,6 +132,18 @@ class PasswordsPage(ctk.CTkFrame):
         self.controller = controller
         label = ctk.CTkLabel(self, text="PasswordsPage")
         label.pack(side="top", fill="x")
+
+
+
+class Details(ctk.CTkFrame):
+    def __init__(self, parent, controller):
+        ctk.CTkFrame.__init__(self, parent)
+        self.configure(fg_color="black")
+        self.name = "Détails"
+        self.controller = controller
+        label = ctk.CTkLabel(self, text="Détails")
+        label.pack(side="top", fill="x")
+
 
 
 a=0
@@ -106,8 +157,80 @@ def mdp(MdpLongueur):
         for i in range(MdpLongueur):
             a=randint(33,126)
             Mdp.append(chr(a))
+        pyperclip.copy("".join(Mdp))
         return Mdp
 
+
+
+def verif(verifmdp):
+    s=0
+    nb=0
+    maj=0
+    min=0
+    spe=0
+    rep=0
+    MM=0
+    NB=0
+    ch = list(verifmdp)
+    CH=[]
+    consecnb=0
+    consecspe=0
+    consecmin=0
+    consecmaj=0
+    s=3*len(ch)
+#donner les points
+    for i in range(len(ch)):
+        CH.append(ord(ch[i]))
+        if CH[i]>=48 and CH[i]<=57:
+            nb=nb+1
+        if CH[i]>=65 and CH[i]<=90:
+            maj=maj+1
+        if CH[i]>=97 and CH[i]<=122:
+            min=min+1
+        if CH[i]>=33 and CH[i]<=47 or CH[i]>=58 and CH[i]<=64 or CH[i]>=91 and CH[i]<=96 or CH[i]>=123 and CH[i]<=126:
+            spe=spe+1
+        else:
+            s=s
+#test des malus
+    for y in range(33,126):
+        if CH.count(y)>1:
+            rep=rep+CH.count(y)-1
+        if spe==0 and nb==0:
+            MM=maj+min-2*(maj+min)
+        if spe==0 and maj==0 and min==0:
+            NB=nb-3*nb
+    for z in range(len(CH)-1):
+        if CH[z]>=48 and CH[z]<=57 and CH[z+1]>=48 and CH[z+1]<=57:
+            consecnb=consecnb+1
+        if CH[z]>=65 and CH[z]<=90 and CH[z+1]>=65 and CH[z+1]<=90:
+            consecmaj=consecmaj+1
+        if CH[z]>=97 and CH[z]<=122 and CH[z+1]>=97 and CH[z+1]<=122:
+            consecmin=consecmin+1
+        if CH[z]>=33 and CH[z]<=47 or CH[z]>=58 and CH[z]<=64 or CH[z]>=91 and CH[z]<=96 or CH[z]>=123 and CH[z]<=126 and CH[z+1]>=33 and CH[z+1]<=47 or CH[z+1]>=58 and CH[z+1]<=64 or CH[z+1]>=91 and CH[z+1]<=96 or CH[z+1]>=123 and CH[z+1]<=126:
+            consecspe=consecspe+1
+        else:
+            s=s
+    s=((s+(nb*2)+maj+min+(spe*4)+MM+NB-(rep**2+consecmaj*2+consecmin*2+consecnb*2+consecspe*2))*100)/8000
+    if s>=1:
+        s=1
+    else:
+        if s<=0:
+            s=0
+        else:
+            s=s
+    return s
+
+
+
+def color(self, s):
+    if s<=0.25:
+        self.progressbar_verif.configure(progress_color="red")
+    if s>0.25 and s<=0.5:
+        self.progressbar_verif.configure(progress_color="orange")
+    if s>0.5 and s<=0.75:
+        self.progressbar_verif.configure(progress_color="yellow")
+    if s>0.75:
+        self.progressbar_verif.configure(progress_color="green")
 
 app = App()
 app.mainloop()
