4da4441380aac05dfa9f970e8a446cd35a075539
Thomas Dolnet <thomas.dolnet@free.fr>
2023-05-04 12:31:48 +0200

    Ajout de la fonction csv

--- mdp.py
+++ mdp.py
@@ -1,4 +1,5 @@
 from random import *
+import csv
 a=0
 x=0
 Mdp=[]
@@ -12,3 +13,18 @@ def mdp(n):
             a=randint(33,126)
             Mdp.append(a)
             print(chr(a), end="")
+
+f = open("données.csv","w", newline="")
+writer = csv.writer(f)
+data = [1,"un", "deux", "trois", "quatre"]
+data2 = [2,"cinq","six","sept","huit"]
+writer.writerow(data)
+writer.writerow(data2)
+f.close()
+
+with open('données.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+
+    list_of_rows = list(csv_reader)
+    print(list_of_rows)
+    print(*list_of_rows[0])
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import customtkinter as ctk
-
+import csv
 
 ctk.set_appearance_mode("dark")
 class Table:
@@ -9,13 +9,26 @@ class Table:
 			for j in range(colonnestotales):
 				self.entry = ctk.CTkEntry(root, width=200,fg_color="white", text_color="black", font=("Calibri",15,"bold"))
 				self.entry.grid(row=i, column=j)
-				self.entry.insert(ctk.END, liste[i][j])
+				self.entry.insert(ctk.END, list_of_rows[i][j])
+
+f = open("données.csv","w", newline="")
+writer = csv.writer(f)
+data = [("un", "deux", "trois", "quatre"),
+		("cinq","six","sept","huit")]
+writer.writerows(data)
+f.close()
+
+with open('données.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+
+    list_of_rows = list(csv_reader)
+    print(list_of_rows)
 
 liste = [(1,"A","B"),
         (2,"C","D")]
 
-lignestotales = len(liste)
-colonnestotales = len(liste[0])
+lignestotales = len(list_of_rows)
+colonnestotales = len(list_of_rows[0])
 
 root = ctk.CTk()
 t = Table(root)
